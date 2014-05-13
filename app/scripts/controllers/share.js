e348a425fd485ab2bb04d7bcd0f7547701e2aac9
Alberto Cole <acpii2005@gmail.com>
2014-05-12 20:29:24 -0600

    Cuadros de amigos a la hora de compartir ahora son directivas

--- app/scripts/controllers/share.js
+++ app/scripts/controllers/share.js
@@ -10,34 +10,4 @@ angular.module('shareLocApp')
     },function(friend){
       $scope.friends.push(friend);
     });
-
-    $scope.enviarMensaje = function(friend){
-
-      var locacion = Coordenadaservice.getLocacionEnviar();
-      console.log(locacion.lat());
-      console.log(locacion.lng());
-      /*Imaginate que FB.ui no deja mandar mensajes en mobile web...
-      * Porque Zucarita? Porque?
-      *
-      * Integration Examples:
-      * This dialog can be used with the JavaScript SDK and by performing a full
-      * redirect to a URL.
-      * It is not supported on mobile devices. -__________-
-      * https://developers.facebook.com/docs/sharing/reference/send-dialog
-      * */
-      var obj = {
-        method: 'feed',
-        to: friend.id,
-        link: 'http://sharelocapi.jit.su/shareloc/dist/#/location/'+locacion.lat()+'/'+locacion.lng(),
-        picture:'http://www.omicrono.com/wp-content/uploads/2011/12/trololo.jpg',
-        name: 'ShareLoc',
-        caption: 'Compartir Ubicación',
-        description: 'Comparte ubicaciones con ShareLoc.'
-      };
-
-      function callback(response) {
-
-      }
-      FB.ui(obj, callback);
-    };
   });
---
+++ app/scripts/directives/friendbox.js
@@ -0,0 +1,54 @@
+'use strict';
+
+angular.module('shareLocApp')
+  .directive('friendbox', function (Coordenadaservice) {
+    return {
+      scope:{
+        friendInfo:'=friendInfo'
+      },
+      templateUrl: function(tElement,tAttrs){
+        return tAttrs.friendTemplate;
+      },
+      /*
+      * 'A' - only matches attribute name
+       'E' - only matches element name
+       'C' - only matches class name
+       These restrictions can all be combined as needed:
+
+       'AEC' - matches either attribute or element or class name
+       */
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        console.log('test');
+        scope.enviarMensaje = function(){
+          var locacion = Coordenadaservice.getLocacionEnviar();
+          console.log(locacion.lat());
+          console.log(locacion.lng());
+          /*Imaginate que FB.ui no deja mandar mensajes en mobile web...
+           * Porque Zucarita? Porque?
+           *
+           * Integration Examples:
+           * This dialog can be used with the JavaScript SDK and by performing a full
+           * redirect to a URL.
+           * It is not supported on mobile devices. -__________-
+           * https://developers.facebook.com/docs/sharing/reference/send-dialog
+           * */
+          var obj = {
+            method: 'feed',
+            to: scope.friendInfo.id,
+            link: 'http://sharelocapi.jit.su/shareloc/dist/#/location/'+locacion.lat()+'/'+locacion.lng(),
+            picture:'http://www.omicrono.com/wp-content/uploads/2011/12/trololo.jpg',
+            name: 'ShareLoc',
+            caption: 'Compartir Ubicación',
+            description: 'Comparte ubicaciones con ShareLoc.'
+          };
+
+          function callback(response) {
+
+          }
+          FB.ui(obj, callback);
+        }
+         //element.text('this is the friendBox directive');
+      }
+    };
+  });
